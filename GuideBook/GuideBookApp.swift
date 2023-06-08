96b96d2c9c24439cbffe6242a2aa8f6a376db7f2
Lukáš Skřivánek <luky.skrivos@gmail.com>
2023-06-08 16:12:02 +0200

    Initial Commit

---
+++ GuideBook/GuideBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideBookApp.swift
+//  GuideBook
+//
+//  Created by macbook on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuideBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
